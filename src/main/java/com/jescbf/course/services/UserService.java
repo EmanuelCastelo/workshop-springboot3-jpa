2e7f059f54535c128391da9c38c960288be864d9
EmanuelCastelo <jescbf@yahoo.com.br>
2024-06-02 20:13:40 -0300

    Service layer, component registration

---
+++ src/main/java/com/jescbf/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.jescbf.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jescbf.course.entities.User;
+import com.jescbf.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
